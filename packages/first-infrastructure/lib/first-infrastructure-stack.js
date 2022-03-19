85d0949ed9be5b13322b96e0c080b8ae8076053b
Artem Devlysh <artem@devlysh.com>
2022-03-19 17:04:05 +0200

    Create first inftactructure stack with first bucket

---
+++ packages/first-infrastructure/lib/first-infrastructure-stack.js
@@ -0,0 +1,20 @@
+const { Stack, RemovalPolicy } = require("aws-cdk-lib");
+const s3 = require("aws-cdk-lib/aws-s3");
+
+class FirstInfrastructureStack extends Stack {
+  /**
+   *
+   * @param {Construct} scope
+   * @param {string} id
+   * @param {StackProps=} props
+   */
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    new s3.Bucket(this, "FirstBucket", {
+      versioned: true,
+    });
+  }
+}
+
+module.exports = { FirstInfrastructureStack };
---
+++ packages/first-infrastructure/test/first-infrastructure.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const FirstInfrastructure = require('../lib/first-infrastructure-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/first-infrastructure-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new FirstInfrastructure.FirstInfrastructureStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
