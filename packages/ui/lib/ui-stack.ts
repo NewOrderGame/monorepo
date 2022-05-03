7eac2512589b5dc44e6fd88f62b5a7910ba6145a
Artem Devlysh <artem@devlysh.com>
2022-05-03 14:18:15 +0300

    Add UiStack

---
+++ packages/ui/lib/ui-stack.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+import { StaticSite } from './static-site';
+import { App, Stack, StackProps } from 'aws-cdk-lib';
+
+/**
+ * This stack relies on getting the domain name from CDK context.
+ * Use 'cdk synth -c domain=mystaticsite.com -c subdomain=www'
+ * Or add the following to cdk.json:
+ * {
+ *   "context": {
+ *     "domain": "mystaticsite.com",
+ *     "subdomain": "www",
+ *     "accountId": "1234567891",
+ *   }
+ * }
+ **/
+export class UiStack extends Stack {
+  constructor(parent: App, name: string, props: StackProps) {
+    super(parent, name, props);
+
+    const domainName = this.node.tryGetContext('domain');
+    const siteSubDomain = this.node.tryGetContext('subdomain');
+
+    new StaticSite(this, 'StaticSite', {
+      domainName,
+      siteSubDomain
+    });
+  }
+}
