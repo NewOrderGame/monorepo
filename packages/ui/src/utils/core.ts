87958407d41ca0275b062846833caeec566c2c79
Artem Devlysh <artem@devlysh.com>
2022-05-19 01:14:33 +0300

    Fix env variables in UI

--- packages/ui/src/utils/core.ts
+++ packages/ui/src/utils/core.ts
@@ -1,9 +1,23 @@
 import { io, Socket } from 'socket.io-client';
 
+if (!process.env.NODE_ENV) {
+  throw new Error('Environment variable NODE_ENV is missing');
+}
+
+if (!process.env.REACT_APP_NOG_CORE_URL_LOCAL) {
+  throw new Error('Environment variable REACT_APP_NOG_CORE_URL_LOCAL is missing');
+}
+
+if (!process.env.REACT_APP_NOG_CORE_URL_PROD) {
+  throw new Error(
+    'Environment variable REACT_APP_NOG_CORE_URL_LOCAL is missing'
+  );
+}
+
 export const CORE_URL =
   process.env.NODE_ENV === 'development'
-    ? 'ws://10.108.1.8:5000'
-    : 'wss://core.newordergame.hochburg.devlysh.com';
+    ? process.env.REACT_APP_NOG_CORE_URL_LOCAL
+    : process.env.REACT_APP_NOG_CORE_URL_PROD;
 
 function core() {
   const auth: Socket = io(`${CORE_URL}/auth`, {
