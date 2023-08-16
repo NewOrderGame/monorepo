d516edf301d2407c3383f823256c158fc0b2ca1c
Artem Devlysh <artem@devlysh.com>
2023-08-16 08:35:30 +0300

    chore: Refactor landing page

--- infrastructure/landing-page/public/index.js
+++ infrastructure/landing-page/public/index.js
@@ -1,116 +1,44 @@
 (() => {
-  const RANDOM_CHARACTERS = [
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'K',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    '0'
-  ];
+  const RANDOM_CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'.split('');
 
   const INTERVAL_TIME = 50;
   const CHANCE = 0.05;
 
-  const intervalId = setInterval(siftContent, INTERVAL_TIME);
-
   const content = document.querySelector('#content');
   const paragraphs = Array.from(content.querySelectorAll('p'));
-  const originalContentHTML = content.innerHTML;
-  const originalText = paragraphs.map((par) => par.innerText);
-  const originalCount = originalText.reduce((a, c) => a + c.length, 0);
+  const originalText = paragraphs.map(p => p.innerText);
+  let randomizedText = randomizeText(originalText);
 
-  let text = randomizeText();
+  const intervalId = setInterval(() => {
+    let matchedCount = 0;
 
-  function siftContent() {
-    let count = 0;
-    text = text.map((paragraph, i) =>
-      paragraph.map((char, j) => {
-        const originalChar = originalText[i][j];
-
-        if (char === originalChar || Math.random() < CHANCE) {
-          count += 1;
-          return originalChar;
+    randomizedText = randomizedText.map((paragraph, i) =>
+      Array.from(paragraph).map((char, j) => {
+        const origChar = originalText[i][j];
+        
+        if (char === origChar || Math.random() < CHANCE) {
+          matchedCount++;
+          return origChar;
         }
-
-        return generateRandomChar();
-      })
+        
+        return RANDOM_CHARACTERS[Math.floor(Math.random() * RANDOM_CHARACTERS.length)];
+      }).join('')
     );
 
-    if (count !== originalCount) {
-      paragraphs.forEach((p, i) => {
-        p.innerText = text[i].join('');
-      });
-    } else {
-      content.innerHTML = originalContentHTML;
+    paragraphs.forEach((p, i) => {
+      p.innerText = randomizedText[i];
+    });
+
+    if (matchedCount === originalText.join('').length) {
       clearInterval(intervalId);
     }
-  }
+  }, INTERVAL_TIME);
 
-  function randomizeText() {
-    return originalText.map((par) =>
-      Array.from(par).map((char) =>
-        RANDOM_CHARACTERS.includes(char) ? generateRandomChar() : char
-      )
+  function randomizeText(texts) {
+    return texts.map(text => 
+      Array.from(text).map(char => 
+        RANDOM_CHARACTERS.includes(char) ? RANDOM_CHARACTERS[Math.floor(Math.random() * RANDOM_CHARACTERS.length)] : char
+      ).join('')
     );
-  }
-
-  function generateRandomChar() {
-    return RANDOM_CHARACTERS[
-      Math.ceil(Math.random() * RANDOM_CHARACTERS.length) - 1
-    ];
   }
 })();
