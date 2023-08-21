ee78b734532564573d1d3aff0b5a63acd91fb4d5
Artem Devlysh <artem@devlysh.com>
2023-08-21 15:37:35 +0300

    feat: add hex direction vectors

---
+++ packages/common/src/lib/hex-direction-vectors.ts
@@ -0,0 +1,18 @@
+import { HexDirectionNumeric } from './enums';
+import { Hexagon } from './hexagon';
+import { HexDirection } from './types';
+
+export class HexDirectionVectors {
+  static vectors: Record<HexDirection, Hexagon> = {
+    [HexDirectionNumeric.N0]: new Hexagon(+1, 0, -1),
+    [HexDirectionNumeric.N1]: new Hexagon(+1, -1, 0),
+    [HexDirectionNumeric.N2]: new Hexagon(0, -1, +1),
+    [HexDirectionNumeric.N3]: new Hexagon(-1, 0, +1),
+    [HexDirectionNumeric.N4]: new Hexagon(-1, +1, 0),
+    [HexDirectionNumeric.N5]: new Hexagon(0, +1, -1)
+  };
+
+  static get(direction: HexDirection): Hexagon {
+    return this.vectors[direction];
+  }
+}
