ca1662c1569911b0ab5dffc32b9abbc549f87517
Artem Devlysh <artem@devlysh.com>
2023-08-26 20:11:04 +0300

    feat(cell): add neighbors

--- packages/common/src/lib/cell.ts
+++ packages/common/src/lib/cell.ts
@@ -3,19 +3,22 @@ import { CellProps } from './cell-props';
 import { Hexagonal } from './hexagonal';
 import { CellActionPermission } from './enums';
 import { CellElement } from './enums';
+import { CubicHex } from './types';
 
 export class Cell extends Hexagon implements Hexagonal {
   actionPermission: CellActionPermission;
   element: CellElement;
+  neighbors: CubicHex[];
 
-  constructor(
-    readonly props: CellProps,
-    x: number,
-    y: number,
-    z?: number
-  ) {
+  constructor(props: CellProps, x: number, y: number, z?: number) {
     super(x, y, z);
     this.actionPermission = props.actionPermission;
     this.element = props.element;
+
+    this.neighbors = [];
+    for (let d = 0; d < Hexagon.DIRECTIONS_QUANTITY; d++) {
+      const neighbor = Hexagon.cubicNeighbor(this, d);
+      this.neighbors[d] = neighbor;
+    }
   }
 }
