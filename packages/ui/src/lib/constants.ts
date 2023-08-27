1b18e9196bb43c59e155ae02ec2a70b9b076c8a5
Artem Devlysh <artem@devlysh.com>
2023-08-27 09:28:24 +0300

    fix(location-site-page, hex-map): find path and reachable appearence

--- packages/ui/src/lib/constants.ts
+++ packages/ui/src/lib/constants.ts
@@ -8,6 +8,10 @@ export const HEXAGON_TEXTURE_HEIGHT = 30;
 
 export const BLACK_HEXAGON: Texture = Texture.from('hexagon-black.png');
 export const WHITE_HEXAGON: Texture = Texture.from('hexagon-white.png');
+export const DARK_GREY_HEXAGON: Texture = Texture.from('hexagon-dark-grey.png');
+export const LIGHT_GREY_HEXAGON: Texture = Texture.from(
+  'hexagon-light-grey.png'
+);
 export const BLACK_TRANSPARENT_HEXAGON: Texture = Texture.from(
   'hexagon-black-transparent.png'
 );
